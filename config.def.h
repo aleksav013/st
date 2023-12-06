ecc0cb9cf3f6fe5be4d0a92d626e8520964ad052
Aleksa Vuckovic <aleksa@vuckovic.cc>
2023-12-06 02:20:29 +0100

    font name update

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono Nerd Font:style=Bold:pixelsize=16:antialias=true:autohint=true";
+static char *font = "JetBrainsMono NFM:style=Bold:pixelsize=16:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 /*    "Liberation Mono:pixelsize=12:antialias=true:autohint=true"; */
